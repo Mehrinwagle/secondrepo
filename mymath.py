7fab0029fb2023b3b43b0ebe48c7ed08147f0d7a
rudraroyaltypython <rudrafuturetech@gmail.com>
2025-08-25 11:13:06 +0530

    mymath added

---
+++ mymath.py
@@ -0,0 +1,20 @@
+class Mymath:
+    def first_value(self,x):
+        self.x=x
+
+    def second_value(self,y):
+        self.y=y 
+
+    def add(self,d):
+        self.d=d
+        self.d=self.x + self.y
+    
+    def result(self):
+        print("The result is : ",self.d)
+
+p=Mymath()
+p.first_value(8)
+p.second_value(9)
+p.add(int)
+p.result()
+
