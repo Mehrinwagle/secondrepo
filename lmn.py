8e0b1a4362396959b86872a3cc969103af4ba337
Mehrinwagle <mehrinwagle26@gmail.com>
2025-08-25 11:09:28 +0530

    math add

---
+++ lmn.py
@@ -0,0 +1,14 @@
+class Math:
+    def one(self,first):
+        self.first=first
+    def two(self,second):
+        self.second=second
+    def add(self):
+        print(self.first+self.second)
+
+e=Math()
+e.one(23)
+e.two(56)
+e.add()
+
+    
